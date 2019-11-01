eaa63d53d406c24a1b0bfb463caa05ff0b39b926
Arthemis <tododkhurelbaatar@gmail.com>
2019-11-01 23:06:30 +0800

    Fix

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button :class="`${type}`">
+    <slot />
+  </button>
+</template>
+<script>
+export default {
+  name: "rt-button",
+  props: {
+    type: {
+      Type: String,
+      Default: "default"
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
-import * as components from './components'
-
+import * as components from "./components";
+import "./sass/main.scss";
 const Arthemis = {
-  install(Vue, options = {}) {
-    // components
+  install(Vue) {
     for (const componentName in components) {
-      const component = components[componentName]
-
-      Vue.component(component.name, component)
+      const component = components[componentName];
+      Vue.component(component.name, component);
     }
   }
-}
+};
 
-export default Arthemis
+export default Arthemis;
 
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(Arthemis)
+if (typeof window !== "undefined" && window.Vue) {
+  window.Vue.use(Arthemis);
 }
